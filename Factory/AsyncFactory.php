ebaf977cbea7e52baf324f7e504817da8f7c1943
jul6art <geoffreyk42@gmail.com>
2021-01-19 04:48:47 +0100

    Update AsyncFactory.php

--- Factory/AsyncFactory.php
+++ Factory/AsyncFactory.php
@@ -18,55 +18,55 @@ final class AsyncFactory implements AsyncFactoryInterface
     public static function create(...$args): Object
     {
         if (\count($args) !== 4) {
-            Throw new \InvalidArgumentException('You need 4 arguments to create an EntityAsyncEvent: string $type, string $class, int $id and ?int $currentUserId');
+            Throw new \InvalidArgumentException('You need 4 arguments to create an EntityAsyncEvent: string $type, string $entityClass, int $entityId and ?int $currentUserId');
         }
 
         return new EntityAsyncEvent($args[0], $args[1], $args[2], $args[3]);
     }
 
     /**
-     * @param string $class
-     * @param int $id
+     * @param string $entityClass
+     * @param int $entityId
      * @param int|null $currentUserId
      * @return EntityAsyncEventInterface
      */
-    public static function createEntityCreatedMessage(string $class, int $id, int $currentUserId = null): EntityAsyncEventInterface
+    public static function createEntityCreatedMessage(string $entityClass, int $entityId, int $currentUserId = null): EntityAsyncEventInterface
     {
         return self::create(EntityAsyncEventType::ENTITY_ASYNC_EVENT_TYPE_CREATED, $class, $id, $currentUserId);
     }
 
 
     /**
-     * @param string $class
-     * @param int $id
+     * @param string $entityClass
+     * @param int $entityId
      * @param int|null $currentUserId
      * @return EntityAsyncEventInterface
      */
-    public static function createEntityDeletedMessage(string $class, int $id, int $currentUserId = null): EntityAsyncEventInterface
+    public static function createEntityDeletedMessage(string $entityClass, int $entityId, int $currentUserId = null): EntityAsyncEventInterface
     {
         return self::create(EntityAsyncEventType::ENTITY_ASYNC_EVENT_TYPE_DELETED, $class, $id, $currentUserId);
     }
 
 
     /**
-     * @param string $class
-     * @param int $id
+     * @param string $entityClass
+     * @param int $entityId
      * @param int|null $currentUserId
      * @return EntityAsyncEventInterface
      */
-    public static function createEntityEditedMessage(string $class, int $id, int $currentUserId = null): EntityAsyncEventInterface
+    public static function createEntityEditedMessage(string $entityClass, int $entityId, int $currentUserId = null): EntityAsyncEventInterface
     {
         return self::create(EntityAsyncEventType::ENTITY_ASYNC_EVENT_TYPE_EDITED, $class, $id, $currentUserId);
     }
 
 
     /**
-     * @param string $class
-     * @param int $id
+     * @param string $entityClass
+     * @param int $entityId
      * @param int $currentUserId
      * @return EntityAsyncEventInterface
      */
-    public static function createEntityViewedMessage(string $class, int $id, int $currentUserId): EntityAsyncEventInterface
+    public static function createEntityViewedMessage(string $entityClass, int $entityId, int $currentUserId): EntityAsyncEventInterface
     {
         return self::create(EntityAsyncEventType::ENTITY_ASYNC_EVENT_TYPE_VIEWED, $class, $id, $currentUserId);
     }
